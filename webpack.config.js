3cc2611e793483fca94a128255271ee529d6fdc6
Robert Autenrieth <robin@dbservice.com>
2017-10-07 23:08:35 +0200

    Add bundle for GitHub pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
-    devtool: 'inline-source-map',
+    // devtool: 'inline-source-map',
     module: {
       rules: [
         {
@@ -18,5 +19,8 @@ module.exports = {
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+      new UglifyJSPlugin()
+    ]
   };
